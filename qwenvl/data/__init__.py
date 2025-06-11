5d076bb1f069c803eca14d47b38ded0c2b8640ad
Xiaowen Zhang <xiaowenn.zhang@mail.utoronto.ca>
2025-06-11 10:01:00 -0400

    Added openpmc_tiny for fast testing

--- qwenvl/data/__init__.py
+++ qwenvl/data/__init__.py
@@ -6,6 +6,7 @@ from .base import BaseDataset
 from .sft import SFTDataset
 from .openbiomedvid import OpenbiomedvidDataset
 from .openpmc import OpenpmcDataset
+from .openpmc_tiny import OpenpmcTinyDataset
 
 from .benchmark import BenchmarkDataset
 from .vqa import VQADataset
@@ -27,6 +28,12 @@ avail_datasets = {
     "ds_dir": "/projects/cft_vlm/datasets/open_pmc/data/dataset",
     "media_dir": None,
     "ds_class": OpenpmcDataset,
+    "ds_key": "vector-institute/open-pmc"
+  },
+  "open-pmc-tiny": {
+    "ds_dir": "/projects/cft_vlm/datasets/open_pmc_tiny/data/dataset",
+    "media_dir": None,
+    "ds_class": OpenpmcTinyDataset,
     "ds_key": "vector-institute/open-pmc"
   },
   "openbiomedvid": {
